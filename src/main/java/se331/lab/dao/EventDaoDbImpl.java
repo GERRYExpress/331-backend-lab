08ad8b631d873df00f7faea728cf0828d7b07b13
GERRY Express <krittameth2020@gmail.com>
2026-09-04 18:48:10 +0700

    feat:(2.5): add EventDaoDbImpl

---
+++ src/main/java/se331/lab/dao/EventDaoDbImpl.java
@@ -0,0 +1,37 @@
+package se331.lab.dao;
+
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Repository;
+import se331.lab.entity.Event;
+import se331.lab.repository.EventRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+@Repository
+@RequiredArgsConstructor
+public class EventDaoDbImpl implements EventDao{
+    final EventRepository eventRepository;
+
+    @Override
+    public Integer getEventSize() {
+        return Math.toIntExact(eventRepository.count());
+    }
+
+    @Override
+    public List<Event> getEvents(Integer pageSize, Integer page) {
+        List<Event> events = eventRepository.findAll();
+        pageSize = pageSize == null ? events.size() : pageSize;
+        page = page == null ? 1 : page;
+        int firstIndex = (page - 1) * pageSize;
+        return events.subList(firstIndex, Math.min(firstIndex + pageSize, events.size()));
+    }
+
+    @Override
+    public Event getEvent(Long id) {
+        return eventRepository.findById(id).orElse(null);
+    }
+}
