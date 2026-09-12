61bf9ebd78969a039ef9fa393f227148a3bf228d
GERRY Express <krittameth2020@gmail.com>
2026-09-12 14:44:02 +0700

    feat(6.3): add save event with organizer

--- src/main/java/se331/lab/dao/OrganizerDao.java
+++ src/main/java/se331/lab/dao/OrganizerDao.java
@@ -4,8 +4,9 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import se331.lab.entity.Organizer;
 
-import java.util.List;
+import java.util.Optional;
 
 public interface OrganizerDao {
     Page<Organizer> getOrganizers(Pageable pageRequest);
+    Optional<Organizer> findById(Long id);
 }
