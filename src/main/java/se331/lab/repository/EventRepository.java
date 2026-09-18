e91e8dfdcbf32862234d952d0491bf78dc4b0249
GERRY Express <krittameth2020@gmail.com>
2026-09-18 19:37:57 +0700

    feat(1.4): add find by partial title

--- src/main/java/se331/lab/repository/EventRepository.java
+++ src/main/java/se331/lab/repository/EventRepository.java
@@ -9,6 +9,6 @@ import java.util.List;
 
 public interface EventRepository extends JpaRepository<Event, Long> {
     List<Event> findAll();
-
     Page<Event> findByTitle(String title, Pageable pageRequest);
+    Page<Event> findByTitleContaining(String title, Pageable pageRequest);
 }
